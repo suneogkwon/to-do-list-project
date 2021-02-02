72cb01f906c9134ee7110d317826a23294f469f9
suneogkwon <tnsdjr7@gmail.com>
2021-02-02 23:30:08 +0900

    Remove TOdoDao constructor

--- src/main/java/com/github/suneogkwon/dao/TodoDao.java
+++ src/main/java/com/github/suneogkwon/dao/TodoDao.java
@@ -3,30 +3,20 @@ package com.github.suneogkwon.dao;
 import com.github.suneogkwon.dto.TodoDto;
 import com.github.suneogkwon.setting.DatabaseInitializer;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
+import java.util.ArrayList;
 import java.util.List;
 
 public class TodoDao {
-    private Connection connection;
-    private Statement statement;
-
-    public TodoDao() {
-        DatabaseInitializer databaseInitializer = new DatabaseInitializer();
-        try {
-            connection = DriverManager.getConnection(databaseInitializer.getDbUrl(),
-                    databaseInitializer.getUser(),
-                    databaseInitializer.getPassword());
-
-            statement = connection.createStatement();
-        } catch (SQLException throwables) {
-            throwables.printStackTrace();
-        }
-    }
 
     public int addTodo(TodoDto todoDto) throws SQLException {
+        DatabaseInitializer databaseInitializer = new DatabaseInitializer();
+        Connection connection = DriverManager.getConnection(databaseInitializer.getDbUrl(),
+                databaseInitializer.getUser(),
+                databaseInitializer.getPassword());
+
+        Statement statement = connection.createStatement();
+
         String insertQuery = "insert into todo(title, name, sequence) values('"
                 + todoDto.getTitle() + "', '"
                 + todoDto.getName() + "', "
@@ -35,10 +25,32 @@ public class TodoDao {
         return statement.executeUpdate(insertQuery);
     }
 
-    public List<TodoDto> getTodo(){
+    public List<TodoDto> getTodo() throws SQLException {
+        DatabaseInitializer databaseInitializer = new DatabaseInitializer();
+        Connection connection = DriverManager.getConnection(databaseInitializer.getDbUrl(),
+                databaseInitializer.getUser(),
+                databaseInitializer.getPassword());
+
+        Statement statement = connection.createStatement();
+
+        List<TodoDto> todoDtoList = new ArrayList<>();
+        String selectQuery = "select id, title, name, sequence, type, regdate from todo order by regdate desc";
+        ResultSet resultSet = statement.executeQuery(selectQuery);
+        while (resultSet.next()) {
+            TodoDto todoDto = new TodoDto();
+            todoDto.setId(resultSet.getLong("id"));
+            todoDto.setTitle(resultSet.getString("title"));
+            todoDto.setName(resultSet.getString("name"));
+            todoDto.setSequence(resultSet.getInt("sequence"));
+            todoDto.setType(resultSet.getString("type"));
+            todoDto.setRegdate(resultSet.getString("regdate"));
+            todoDtoList.add(todoDto);
+        }
+
+        return todoDtoList;
     }
 
-    public int updateTodo(TodoDto todoDto){
+    public int updateTodo(TodoDto todoDto) {
 
     }
 }
