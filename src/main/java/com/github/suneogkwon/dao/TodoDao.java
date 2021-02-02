b13eb314fb746adface1c0c8c05b23cb004a52f1
suneogkwon <tnsdjr7@gmail.com>
2021-02-02 22:45:18 +0900

    Add TodoFormServlet

--- src/main/java/com/github/suneogkwon/dao/TodoDao.java
+++ src/main/java/com/github/suneogkwon/dao/TodoDao.java
@@ -2,7 +2,20 @@ package com.github.suneogkwon.dao;
 
 import com.github.suneogkwon.dto.TodoDto;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
 import java.util.List;
 
 public class TodoDao {
+    public int addTodo(TodoDto todoDto){
+
+    }
+
+    public List<TodoDto> getTodo(){
+
+    }
+
+    public int updateTodo(TodoDto todoDto){
+
+    }
 }
---
+++ src/main/java/com/github/suneogkwon/servlet/TodoFormServlet.java
@@ -0,0 +1,17 @@
+package com.github.suneogkwon.servlet;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("/todoform")
+public class TodoFormServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+
+    }
+}
